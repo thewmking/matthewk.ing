a1f529ef9d115edd82a408503e37a47d6c986c40
Matthew King <thewmking@gmail.com>
2024-04-22 22:40:21 -0400

    Add skills icons

---
+++ src/components/SkillIcon.vue
@@ -0,0 +1,11 @@
+<script setup>
+defineProps({
+  iconSrc: { type: String, default: null }
+});
+</script>
+
+<template>
+  <div class="skill-icon-wrapper">
+    <img :src="iconSrc" />
+  </div>
+</template>
--- src/components/SkillsCard.vue
+++ src/components/SkillsCard.vue
@@ -1,5 +1,66 @@
 <script setup>
-import TileCard from '@/components/TileCard.vue'
+import TileCard from '@/components/TileCard.vue';
+import SkillIcon from '@/components/SkillIcon.vue';
+
+import awsIcon from '@/assets/icons/aws.png';
+import jsIcon from '@/assets/icons/js.png';
+import nginxIcon from '@/assets/icons/nginx.png';
+import postgresIcon from '@/assets/icons/postgres.png';
+import railsIcon from '@/assets/icons/rails.png';
+import reactIcon from '@/assets/icons/react.png';
+import rubyIcon from '@/assets/icons/ruby.png';
+import vueIcon from '@/assets/icons/vue.png';
+
+const skillItems = [
+  {
+    name: 'Ruby',
+    score: 5,
+    icon: rubyIcon,
+    hex: '',
+  },
+  {
+    name: 'Ruby on Rails',
+    score: 5,
+    icon: railsIcon,
+    hex: '',
+  },
+  {
+    name: 'Javascript',
+    score: 5,
+    icon: jsIcon,
+    hex: '',
+  },
+  {
+    name: 'Vue.js',
+    score: 5,
+    icon: vueIcon,
+    hex: '',
+  },
+  {
+    name: 'PostgreSQL',
+    score: 4,
+    icon: postgresIcon,
+    hex: '',
+  },
+  {
+    name: 'React JS',
+    score: 3,
+    icon: reactIcon,
+    hex: '',
+  },
+  {
+    name: 'AWS',
+    score: 3,
+    icon: awsIcon,
+    hex: '',
+  },
+  {
+    name: 'nginx',
+    score: 3,
+    icon: nginxIcon,
+    hex: '',
+  },
+];
 </script>
 
 <template>
@@ -7,14 +68,11 @@ import TileCard from '@/components/TileCard.vue'
     <template #body>
       <h3>Positions:</h3>
       <ul>
-        <li>Ruby - 5</li>
-        <li>Ruby on Rails - 5</li>
-        <li>Javascript - 5</li>
-        <li>Vue.js - 5</li>
-        <li>React.js - 3</li>
-        <li>AWS - 3</li>
-        <li>PostgreSQL - 4</li>
-        <li>Nginx - 3</li>
+        <li v-for="item in skillItems" :key="item.name" class="skill-item">
+          <SkillIcon :icon-src="item.icon" />
+          <span class="skill-item-name">{{ item.name }}</span>
+          <div class="skill-item-score">{{ item.score }}</div>
+        </li>
       </ul>
     </template>
   </TileCard>
